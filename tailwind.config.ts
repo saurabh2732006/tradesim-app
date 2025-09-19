7413813d9dc551470d73dc6170a53ef4adf961c8
Saurabh Kumar <saurabh2732006@gmail.com>
2025-09-19 17:57:34 +0000

    restart the app and show  me animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -102,7 +102,7 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'fade-in-up': 'fade-in-up 0.5s ease-out forwards',
+        'fade-in-up': 'fade-in-up 1s ease-out forwards',
       },
       transitionDelay: {
         '200': '200ms',
